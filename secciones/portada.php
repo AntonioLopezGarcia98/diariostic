3b4ddf35defa1c18eef6361634bc576ff38b5a47
AntonioLopezGarcia98 <64582410+AntonioLopezGarcia98@users.noreply.github.com>
2020-11-05 22:42:40 +0100

    cambio de titulo para ver si funciona en Heraku

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada modificada para ver si funciona",
+"titulo" => "Portada nueva modificada por antonio",
 "autor" => "mtorres",
 "resumen" => "Resumen de la portada",
 ];
